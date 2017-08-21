2eb5a3ea35459922c223e86cf613a21dcfce85b7
AlB <albert.burc@gmail.com>
2017-08-21 16:35:58 -0400

    finished appeding html for successful and unsuccessful AJAX requests

--- ajax.js
+++ ajax.js
@@ -12,4 +12,28 @@ button12.addEventListener('click', function () {
   });
 })
 
+var button3456 = document.getElementById('button_step3456')
+
+button3456.addEventListener('click', function () {
+
+  var request3456 = $.ajax({
+    url: 'http://first-ajax-api.herokuapp.com/pong',
+    method: 'GET',
+    dataType: 'text'
+  }).done(function (data) {
+    console.log(data)
+    var myElement = document.createElement( 'div' );
+    var step3456 = document.querySelector('#step3456');
+            myElement.innerHTML = data;
+            step3456.append( myElement );
+  }).fail( function( jqXHR, textStatus, errorThrown ) {
+            console.log( 'REQUEST FAILED' );
+            // console.log( textStatus, errorThrown );
+
+            var msg = document.createElement( 'p' );
+            msg.innerText = 'Whoops, something went wrong!';
+            document.body.append( msg );
+  })
+
+});
 });
