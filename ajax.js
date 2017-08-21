ec01455fc42b59edde27ae59e3c2e9a8530d05e7
AlB <albert.burc@gmail.com>
2017-08-21 15:58:14 -0400

    first ajax request done

--- ajax.js
+++ ajax.js
@@ -1,5 +1,15 @@
 document.addEventListener("DOMContentLoaded", function() {
 
   /* Your code goes here */
+var button12 = document.getElementById('button_step12')
+
+button12.addEventListener('click', function () {
+
+  var request12 = $.ajax({
+    url: 'http://first-ajax-api.herokuapp.com/a_car',
+    method: 'GET',
+    dataType: 'text'
+  });
+})
 
 });
